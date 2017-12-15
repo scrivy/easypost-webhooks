ffe14f7ced71b96efaddad2e0ae767bc3f926f2e
Daniel Scrivano <daniel@danielscrivano.com>
2017-12-15 00:40:51 -0800

    sends emails

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require 'mail'
+require 'safe_yaml'
 
-get '/' do
-	'Hello world!'
+config = SafeYAML.load_file('config.yaml')
+
+mail_options = {
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:domain	=> 'gmail.com',
+	:user_name => config['username'],
+	:password => config['password'],
+	:authentication => 'plain',
+	:enable_starttls_auto => true
+}
+
+Mail.defaults do
+  delivery_method :smtp, mail_options
+end
+
+post '/api/v1/easypost/webhook' do
+	mail = Mail.new do
+		from config['from']
+		to config['to']
+		subject 'easypost webhook'
+	end
+	mail['body'] = request.body.read.to_s
+	mail.deliver!
 end
